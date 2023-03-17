9f91a07cb008465894ebaf24e7322a0a054c12a1
Tadnguyen <jokermr199x@gmail.com>
2023-03-17 14:11:55 +0700

    update(pageReducer): del console log

--- src/redux/reducer/pageReducer.js
+++ src/redux/reducer/pageReducer.js
@@ -7,11 +7,9 @@ const initialState = {
   const pageReducer = (state = initialState, action) => {
     switch (action.type) {
       case types.SET_USERS_PAGE: {
-        console.log(action,'ac');
         return { ...state, usersPage: action.payload.data };
       }
       case types.SET_USERS_PER_PAGE: {
-        console.log(action,'acc');
         return { ...state, usersPerPage: action.payload.data };;
       }
       default:
